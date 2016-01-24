b54b95012bc0025ec871308722d89ca474aa994c
Gabor Raski <gabor.raski@gmail.com>
2016-01-24 12:23:50 +0000

    something working with old version

--- public/javascripts/showlist.js
+++ public/javascripts/showlist.js
@@ -1,17 +1,67 @@
+var imgMonsterARun = new Image();
+var numberOfImagesLoaded = 0;
+
+
 function init() {
     console.log("hello now");
     var stage = new createjs.Stage("demoCanvas");
-    var circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-    circle.x = 100;
-    circle.y = 100;
-    stage.addChild(circle);
-    createjs.Tween.get(circle, { loop: true })
-        .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
-        .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
-        .to({ alpha: 0, y: 225 }, 100)
-        .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
-        .to({ x: 100 }, 800, createjs.Ease.getPowInOut(2));
+
+
     createjs.Ticker.setFPS(60);//frame per second of the animation
     createjs.Ticker.addEventListener("tick", stage);//start the animation
+
+
+    imgMonsterARun.onload = handleImageLoad;
+    imgMonsterARun.onerror = handleImageError;
+    imgMonsterARun.src = "images/MonsterARun.png";
+
+
+    // create spritesheet and assign the associated data.
+    var spriteSheet = new createjs.SpriteSheet({
+        // image to use
+        images: [imgMonsterARun],
+        // width, height & registration point of each sprite
+        frames: {width: 64, height: 64, regX: 32, regY: 32},
+        animations: {
+            walk: [0, 9, "walk"]
+        }
+    });
+
+
+// create a BitmapAnimation instance to display and play back the sprite sheet:
+    bmpAnimation = new createjs.BitmapAnimation(spriteSheet);
+
+// start playing the first sequence:
+    bmpAnimation.gotoAndPlay("walk");     //animate
+
+// set up a shadow. Note that shadows are ridiculously expensive. You could display hundreds
+// of animated rats if you disabled the shadow.
+    bmpAnimation.shadow = new createjs.BitmapAnimation("#454", 0, 5, 4);
+
+    bmpAnimation.name = "monster1";
+    bmpAnimation.direction = 90;
+    bmpAnimation.vX = 4;
+    bmpAnimation.x = 16;
+    bmpAnimation.y = 32;
+
+// have each monster start at a specific frame
+    bmpAnimation.currentFrame = 0;
+    stage.addChild(bmpAnimation);
+
+}
+
+function handleImageError(e) {
+    console.log(e);
+}
+
+function handleImageLoad(e) {
+    numberOfImagesLoaded++;
+
+    // We're not starting the game until all images are loaded
+    // Otherwise, you may start to draw without the resource and raise
+    // this DOM Exception: INVALID_STATE_ERR (11) on the drawImage method
+    if (numberOfImagesLoaded == 2) {
+        numberOfImagesLoaded = 0;
+        //startGame();
+    }
 }
